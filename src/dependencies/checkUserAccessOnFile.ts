24b018fb6d51dfa48b9d6c1d27c4d03ae6810991
Axel DENIS <axel.denis@epitech.eu>
2023-02-04 18:35:43 +0100

    [ADD] Added files route to list files and directories from a path (if authorized). Use the new listFiles.ts that uses checkUserAccess

--- src/dependencies/checkUserAccessOnFile.ts
+++ src/dependencies/checkUserAccessOnFile.ts
@@ -4,6 +4,10 @@ interface UserPathsData {
   [name: string]: string[]
 }
 
+/*
+  Check that user have access on a file or folder by checking usersFoldersAccess.json
+  Also return false if file or folder does not exists
+*/
 export const checkUserAccessOnFile = async (username: string, path: string): Promise<boolean> => {
   let authorized_paths: UserPathsData;
   try {
@@ -18,7 +22,12 @@ export const checkUserAccessOnFile = async (username: string, path: string): Pro
 
   for (let elem of authorized_paths[username]) { // vérification de l'accès
     if (path.startsWith(elem))
-      return true;
+      try {
+        await fs.access(path);
+        return true;
+      } catch {
+        return false;
+      }
   }
   return false;
 }

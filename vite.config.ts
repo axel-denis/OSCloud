b531fd86baad96fc05a1255f122a06503f3df65b
Arthur Aillet <arthur.aillet@epitech.eu>
2023-02-12 18:27:43 +0100

    [FIX] commented fix to limit impact on users outside of wls2 context

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()]
+  // If using wsl2 outside wsl2 file system
+  /*,
   server: {
     watch: {
       usePolling: true
     }
-  }
+  }*/
 });

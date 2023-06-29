e9c37fcc8ac9705d8d7d86a1fbcfafa2531c5a4b
Aillet <arthur.aillet@epitech.eu>
2023-06-29 12:01:25 +0200

    feat(Rust): access to database from file

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod users;
 use serde::{Deserialize, Serialize};
 use users::User;
 use dotenv::dotenv;
+use std::fs::File;
+use std::io::Read;
 
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(transparent)]
@@ -11,31 +13,11 @@ struct Network {
 
 fn main() {
     dotenv().ok();
+    let mut users_file = File::open("database/users.json").unwrap();
+    let mut data = String::new();
+    users_file.read_to_string(&mut data).unwrap();
 
-    let json = r#"
-    [
-      {
-        "id": 0,
-        "name": "Axel",
-        "type": "admin",
-        "password": "password"
-      },
-      {
-        "id": 1,
-        "name": "Arthur",
-        "type": "user",
-        "password": "mdp"
-      },
-      {
-        "id": 2,
-        "name": "test",
-        "type": "user",
-        "password": "test"
-      }
-    ]
-    "#;
-
-    let network: Network = serde_json::from_str(json).unwrap();
+    let network: Network = serde_json::from_str(&data).unwrap();
     let token = std::env::var("ACCESS_TOKEN_SECRET").expect("ACCESS_TOKEN_SECRET must be set.");
 
     println!("{:?} {:?}", network, token);

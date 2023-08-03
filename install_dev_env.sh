e3d4781fe0f89e1442ba4f692b74f03811f24dda
Axel DENIS <axel.denis@epitech.eu>
2023-08-03 18:02:24 +0200

    feat(install_dev_env.sh) added base data generation

--- install_dev_env.sh
+++ install_dev_env.sh
@@ -1,5 +1,31 @@
-rustup update
-rustup component add clippy
-rustup component add rustfmt
-cargo install --force cargo-make
-cargo install --force cargo-watch
+echo "Would you like to install all (y) or just sample data (n) ?"
+
+read yesorno
+
+if [ "$yesorno" = y ]; then
+  rustup update
+  rustup component add clippy
+  rustup component add rustfmt
+  cargo install --force cargo-make
+  cargo install --force cargo-watch
+fi
+
+mkdir -p database
+printf "[
+  {
+    \"id\": 0,
+    \"name\": \"Axel\",
+    \"type\": \"admin\",
+    \"password\": \"password\"
+  },
+  {
+    \"id\": 1,
+    \"name\": \"Arthur\",
+    \"type\": \"user\",
+    \"password\": \"mdp\"
+  }
+]" > database/users.json
+
+printf "ACCESS_TOKEN_SECRET=$(uuidgen)" > ".env"
+echo "installed. Your actual access token secret is an uuid. \
+You can change it to your own if needed"

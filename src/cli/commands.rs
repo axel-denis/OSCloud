0f54b551e7aa1192e2e776c8cc3fb215e57d6870
Aillet <arthur.aillet@epitech.eu>
2023-08-06 17:59:23 +0200

    feat(Back/CLI): cli looks better, commands cleaned

--- src/cli/commands.rs
+++ src/cli/commands.rs
@@ -2,24 +2,38 @@ use std::collections::HashMap;
 
 use crate::database::UserDatabase;
 
-pub type CommandsFn = fn(&UserDatabase) -> ();
+pub type CommandsFn = fn(Vec<String>, &UserDatabase) -> ();
 
 pub type CommandsMap = HashMap<String, CommandsFn>;
 
 pub(crate) fn create_commands_map() -> CommandsMap {
     let mut map = CommandsMap::new();
 
+    map.insert("h".to_owned(), help);
     map.insert("help".to_owned(), help);
+    map.insert("u".to_owned(), debug_users);
     map.insert("users".to_owned(), debug_users);
+    map.insert("e".to_owned(), exit);
+    map.insert("exit".to_owned(), exit);
     map
 }
 
-pub(crate) fn debug_users(db: &UserDatabase) {
+pub(crate) fn debug_users(_: Vec<String>, db: &UserDatabase) {
     db.pretty_print()
 }
 
-pub(crate) fn help(_: &UserDatabase) {
-    println!("Help:");
-    println!("help: display the help");
-    println!("users: display the list of users")
+pub(crate) fn help(_: Vec<String>, _: &UserDatabase) {
+    println!("Usage:");
+    println!("    help, h\tDisplay this usage message");
+    println!("    users, u\tDisplay the list of users in the database and all the data they hold\n");
+    println!("    exit, e\tClose the program and save the current data base");
+    print!("See 'help <command>' for more information on a specific command.\n");
+}
+
+pub(crate) fn exit(_: Vec<String>, db: &UserDatabase) {
+    if let Err(err) = db.save_to_json() {
+        println!("{err}");
+    } else {
+        std::process::exit(0);
+    }
 }
--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,6 +1,6 @@
 mod commands;
 
-use std::io::BufRead;
+use std::io::{BufRead, Write};
 use std::thread;
 use colored::Colorize;
 
@@ -11,15 +11,20 @@ pub fn start_cli(db: &UserDatabase) {
     let db = db.clone();
     thread::spawn(move || {
         println!("Write {} to get the different commands", "help".bold());
+        print!("{} #$ ", "OsCloud".cyan());
+        std::io::stdout().flush().unwrap();
+
         let map: CommandsMap = commands::create_commands_map();
         loop {
             for maybe_line in std::io::stdin().lock().lines() {
                 if let Ok(line) = maybe_line {
                     match map.get(&line) {
-                        Some(commands) => commands(&db),
+                        Some(commands) => commands(Vec::new(), &db),
                         None => println!("Unreconnized cmd!"),
                     }
                 }
+                print!("{} #$ ", "OsCloud".cyan());
+                std::io::stdout().flush().unwrap();
             }
         }
     });

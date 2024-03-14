34e4be86ff977358d850436ae4dde03c2d195a12
Axel DENIS <axel.denis@safrangroup.com>
2024-03-14 11:20:53 +0100

    feat(axum): created a base main with a copy of the old routes (that actually all return hello world)

--- Back/src/main.rs
+++ Back/src/main.rs
@@ -1,21 +1,46 @@
-mod auth_middleware;
-#[cfg(feature = "cli")]
-mod cli;
-mod database;
-mod jwt_manager;
-mod network;
-mod services;
+// mod auth_middleware;
+// #[cfg(feature = "cli")]
+// mod cli;
+// mod database;
+// mod jwt_manager;
+// mod network;
+// mod services;
 
-use database::UserData;
-use dotenv::dotenv;
+// use database::UserData;
+// use dotenv::dotenv;
 
-fn main() {
-    dotenv().ok();
+// fn main() {
+//     dotenv().ok();
 
-    std::env::set_var("RUST_LOG", "actix_web=debug");
-    let userdata = UserData::new();
+//     std::env::set_var("RUST_LOG", "actix_web=debug");
+//     let userdata = UserData::new();
 
-    #[cfg(feature = "cli")]
-    cli::start_cli(&userdata);
-    network::launch_actix(userdata).expect("actix launch crashed");
+//     #[cfg(feature = "cli")]
+//     cli::start_cli(&userdata);
+//     network::launch_actix(userdata).expect("actix launch crashed");
+// }
+
+use axum::{routing::delete, routing::get, routing::post, Router};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/login", post(|| async { "Hello, World!" }))
+        .route("/user", post(|| async { "Hello, World!" }))
+        .route("/user", delete(|| async { "Hello, World!" }))
+        .route("/user", get(|| async { "Hello, World!" }))
+        .route("/save", post(|| async { "Hello, World!" }))
+        .route("/import", post(|| async { "Hello, World!" }))
+        .route("/file", post(|| async { "Hello, World!" }))
+        .route("/home", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
+        .await
+        .expect("failed to launch server");
+    axum::serve(listener, app)
+        .await
+        .expect("failed to launch server");
 }

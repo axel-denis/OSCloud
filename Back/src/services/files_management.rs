8aa8e38fe525e845aa609176ea28df43ffd09395
Axel DENIS <axel.denis@safrangroup.com>
2024-03-29 09:48:05 +0100

    fix(files_management): corrected file rename function to account the new file name (yes it's better)

--- Back/src/services/files_management.rs
+++ Back/src/services/files_management.rs
@@ -31,8 +31,10 @@ pub async fn rename_file(
         return StatusCode::UNAUTHORIZED.into_response();
     };
 
-    //cheking that file name is valid & accessible
-    let new_path = if let Some(np) = base_path.path().to_str() {
+    //cheking that new file name is valid & accessible
+    let mut new_path_draft = base_path.path().clone();
+    new_path_draft.set_file_name(req.new_name);
+    let new_path = if let Some(np) = new_path_draft.to_str() {
         if let Some(new_path) = verifiy_user_path(&app_state.userdata, &np.to_string(), local_user)
         {
             new_path
@@ -91,3 +93,55 @@ pub async fn delete_file(
         None => StatusCode::UNAUTHORIZED.into_response(),
     }
 }
+
+/*
+#[derive(Debug, Deserialize)]
+pub struct FileMoveRequest {
+    file_path: String,
+    new_path: String,
+}
+
+pub async fn move_file(
+    State(app_state): State<Arc<AppState>>,
+    Extension(local_user): Extension<User>,
+    Json(req): Json<FileMoveRequest>,
+) -> Response {
+    // checking that base path is file
+    let base_path = if let Some(user_path) =
+        verifiy_user_path(&app_state.userdata, &req.file_path, local_user.clone())
+    {
+        user_path
+    } else {
+        return StatusCode::UNAUTHORIZED.into_response();
+    };
+
+    //cheking that file name is valid & accessible
+    let new_path = if let Some(np) = base_path.path().to_str() {
+        if let Some(new_path) = verifiy_user_path(&app_state.userdata, &np.to_string(), local_user)
+        {
+            new_path
+        } else {
+            return (StatusCode::UNAUTHORIZED, "Innaccessible file name").into_response();
+        }
+    } else {
+        return (StatusCode::BAD_REQUEST, "Invalid file name").into_response();
+    };
+
+    // Checking for conflicts
+    if !base_path.path().exists() {
+        return (StatusCode::NOT_FOUND, "Given file to rename does not exist").into_response();
+    };
+    if new_path.path().exists() {
+        return (
+            StatusCode::CONFLICT,
+            "Given new file name already exists for another file",
+        )
+            .into_response();
+    };
+
+    match fs::rename(base_path.path(), new_path.path()) {
+        Ok(_) => StatusCode::OK.into_response(),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()).into_response(),
+    }
+}
+*/
